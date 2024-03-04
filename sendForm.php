fad7f558a2a5d1683edfd8366018247313b04210
Rosen Marinov <rosen1710@abv.bg>
2024-03-04 12:19:14 +0200

    Improved sendForm.php file

--- sendForm.php
+++ sendForm.php
@@ -2,7 +2,7 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
-$message = $_POST["message"]."\n\n\n".$name."\n\nТова съобщение е изпратено през формата на сайта на WeMatch";
+$message = $_POST["message"]."\n\n\n".$name."\n\nThis message was sent with the form in the WeMatch website";
 $to = "rosen.i.marinov.2020@elsys-bg.org";
 $headers = "From: " . $name . " <" . $email . ">" . "\r\n" . "Bcc: rosen1710@abv.bg";
 mail($to,$subject,$message,$headers);
