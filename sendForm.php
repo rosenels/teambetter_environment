7a9aca46db45e100745524ce60a02f3235ca9934
Rosen Marinov <rosen1710@abv.bg>
2024-03-04 21:25:13 +0200

    Email encoding problem fixed

--- sendForm.php
+++ sendForm.php
@@ -1,10 +1,22 @@
 <?php
 $name = $_POST["name"];
 $email = $_POST["email"];
-$subject = $_POST["subject"];
-$message = $_POST["message"]."\n\n\n".$name."\n\nThis message was sent with the form in the WeMatch website";
+$subject = mb_encode_mimeheader($_POST["subject"], "utf-8");
+// $message = $_POST["message"]."\n\n\n".$name."\n\nThis message was sent with the form in the WeMatch website";
+$message = '
+<html>
+<body>
+  <p>' . $_POST["message"] . '</p>
+  <br>
+  <br>
+  <p>' . $name . '</p>
+  <br>
+  <p>Това съобщение е изпратено през формата на сайта на WeMatch</p>
+</body>
+</html>
+';
 $to = "rosen.i.marinov.2020@elsys-bg.org";
-$headers = "From: " . $name . " <" . $email . ">" . "\r\n" . "Bcc: rosen1710@abv.bg";
+$headers = "From: " . $name . " <" . $email . ">" . "\r\n" . "Bcc: rosen1710@abv.bg" . "\r\n" . "Content-Type: text/html; charset=utf-8";
 mail($to,$subject,$message,$headers);
 header("Location: thankyou.html");
 ?>
