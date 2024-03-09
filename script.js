8bdb9cba252727fbf11ab27182098ae5cf0c43b0
Rosen Marinov <rosen1710@abv.bg>
2024-03-09 22:11:12 +0200

    Navbar script modification

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function fix_navbar() {
     elements = document.getElementsByClassName("nav-links");
     for (i = 0; i < elements.length; i++) {
-        if (elements[i].getBoundingClientRect().right > window.outerWidth || elements[i].getBoundingClientRect().bottom < 0) {
+        elements[i].style.visibility = "hidden";
+        elements[i].style.display = "flex";
+        if (elements[i].getBoundingClientRect().right > window.outerWidth) {
             elements[i].style.display = "none";
         }
         else {
-            elements[i].style.display = "flex";
+            elements[i].style.visibility = "visible";
         }
     }
 }
